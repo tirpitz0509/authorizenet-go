53db60264b0fc69e7ad4a4b8c42b2bd1953dad9d
bwdxbfzco <55143036+bwdxbfzco@users.noreply.github.com>
2025-06-25 17:14:01 +0400

    validationMode added to profile struct

--- customer_profile.go
+++ customer_profile.go
@@ -354,6 +354,7 @@ type Profile struct {
 	Shipping           *Address         `json:"address,omitempty"`
 	CustomerAddressId  string           `json:"customerAddressId,omitempty"`
 	PaymentProfile     *PaymentProfile  `json:"paymentProfile,omitempty"`
+	ValidationMode     bool             `json:"validationMode,omitempty"`
 }
 
 type PaymentProfiles struct {
